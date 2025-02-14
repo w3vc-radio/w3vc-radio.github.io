1c55f109561ca5dda971735becde31c31a1ba22d
Kandasamy Chokkalingam <kchokkal@andrew.cmu.edu>
2025-02-14 04:19:35 -0500

    Revamp

--- src/stop.svelte
+++ src/stop.svelte
@@ -8,20 +8,24 @@
 		followRecorded = $bindable(),
 		prevFollowRecorded,
 		name,
-		didID = $bindable()
+		undoList = $bindable(),
+		didID = $bindable(),
+		idx
 	} = $props();
 	let buggyLength = $derived(buggies.length.valueOf());
 	let timeFormat = { hour: '2-digit' };
 	let canRecordBuggy = $derived(
-		!followRecorded && buggyLength < Math.min(5, maxBuggies - prevFollowRecorded)
+		!followRecorded && buggyLength < Math.min(6, maxBuggies - prevFollowRecorded)
 	);
 	let canRecordFollow = $derived(!followRecorded && prevFollowRecorded);
+	let shouldRecordFollow = $state(false);
 	let saveBuggy = (event) => {
 		if (canRecordBuggy) {
 			buggies.push({
 				time: new Date(),
 				number: buggies.length + 1
 			});
+			undoList.push(idx);
 		}
 	};
 
@@ -32,10 +36,11 @@
 				number: 'F'
 			});
 			followRecorded = true;
+			undoList.push(idx);
 		}
 	};
 
-	let removeLastBuggy = () => {
+	export const removeLastBuggy = () => {
 		if (followRecorded) {
 			followRecorded = false;
 		}
@@ -53,59 +58,63 @@
 			let diff = buggy.time - prevBuggy.time;
 			let seconds = (diff / 1000) % 60;
 			let minutes = (diff / 1000 - seconds) / 60;
-			return `+${minutes}:${String(Math.ceil(seconds)).padStart(2, '0')}`;
+			return `+${minutes}:${String(Math.floor(seconds)).padStart(2, '0')}`;
 		}
 	};
 
 	let captureIDed = () => {
 		didID = !didID;
 	};
-
 </script>
 
-<div class="row-span-1 grid grid-flow-row grid-cols-12">
-	<div class="col-span-1 [writing-mode:vertical-lr] [text-orientation:sideways]">
-		<p class="rotate-180 text-center touch-manipulation">{name}</p>
+<div class="grid shrink-0 basis-1/3 grid-flow-row grid-cols-12 py-3">
+	<div class="col-span-1 [text-orientation:sideways] [writing-mode:vertical-lr]">
+		<p class="rotate-180 touch-manipulation text-center">{name}</p>
 	</div>
-	<div class="col-span-7 grid grid-flow-col grid-cols-2 grid-rows-3 gap-1 p-1">
+	<div
+		class="col-span-7 grid grid-flow-col grid-cols-2 grid-rows-3 gap-x-1 gap-y-1.5 overflow-hidden p-1"
+	>
 		{#each buggies as buggy (buggy.number)}
-			<div class="text-light flex flex-row justify-around space-x-1 rounded bg-base-300 shadow-xl px-1">
-				<div class="text-md font-mono italic my-auto">{buggy.number}</div>
-				<div class="text-md my-auto font-mono">{formatTime(buggy)}</div>
+			<div
+				class="text-light bg-base-300 flex flex-initial flex-row justify-around space-x-1 rounded-sm px-1 shadow-xl"
+			>
+				<div class="my-auto font-mono text-xl italic">{buggy.number}</div>
+				<div class="my-auto font-mono text-xl">{formatTime(buggy)}</div>
 			</div>
 		{/each}
-		<div class="col-span-1 col-start-2 row-span-1 row-start-3">
-			<button
-				onclick={removeLastBuggy}
-				class="h-full max-h-full w-full max-w-full rounded-btn bg-neutral text-center text-accent transition ease-in-out active:scale-[0.9] touch-manipulation"
-				>Undo</button
-			>
-		</div>
 	</div>
 	<div class="col-span-4 mx-2 flex flex-col space-y-1">
-		<div class="flex flex-grow flex-row gap-2">
+		<div class="flex grow-10 flex-row gap-2">
 			<button
-				use:press={{ timeframe: 300 }}
-				onpress={saveFollow}
-				use:tap={{ timeframe: 300 }}
-				ontap={saveBuggy}
+				use:press={{ timeframe: 300, triggerBeforeFinished: true }}
+				onpress={() => {
+					shouldRecordFollow = true;
+				}}
+				onclick={() => {
+					if (shouldRecordFollow) {
+						saveFollow();
+						shouldRecordFollow = false;
+					} else {
+						saveBuggy();
+					}
+				}}
 				disabled={!canRecordBuggy && !canRecordFollow}
 				class:btn-primary={canRecordBuggy || canRecordFollow}
-				class="btn h-full flex-grow text-lg"
-				>Record {canRecordBuggy ? buggies.length + 1 : 'F'}</button
+				class="btn h-full grow text-lg"
+				>Record {canRecordBuggy && !shouldRecordFollow ? buggies.length + 1 : 'F'}</button
 			>
 
 			<!-- <button -->
 			<!-- 	onclick={saveFollow} -->
 			<!-- 	disabled={!canRecordFollow} -->
 			<!-- 	class:btn-primary={canRecordFollow} -->
-			<!-- 	class="btn h-full flex-grow text-lg">F</button -->
+			<!-- 	class="btn h-full grow text-lg">F</button -->
 			<!-- > -->
 		</div>
 		<button
 			class:hidden={idHidden}
 			onclick={captureIDed}
-			class="btn {didID ? 'btn-success' : 'btn-secondary'} max-h-3 flex-grow text-xl"
+			class="btn {didID ? 'btn-success' : 'btn-secondary'} grow-1 text-xl"
 			>{didID ? "ID'd" : 'ID?'}</button
 		>
 	</div>
---
+++ src/useLocalStorage.svelte.ts
@@ -0,0 +1,39 @@
+import { browser } from '$app/environment';
+
+export class LocalStore<T> {
+	value = $state<T>() as T;
+	key = '';
+
+	constructor(key: string, value: T) {
+		this.key = key;
+		this.value = value;
+
+		if (browser) {
+			const item = localStorage.getItem(key);
+			if (item) this.value = this.deserialize(item);
+		}
+
+		$effect(() => {
+			localStorage.setItem(this.key, this.serialize(this.value));
+		});
+	}
+
+	update() {
+		if (browser) {
+			const item = localStorage.getItem(this.key);
+			if (item) this.value = this.deserialize(item);
+		}
+	}
+
+	serialize(value: T): string {
+		return JSON.stringify(value);
+	}
+
+	deserialize(item: string): T {
+		return JSON.parse(item);
+	}
+}
+
+export function localStore<T>(key: string, value: T) {
+	return new LocalStore(key, value);
+}
